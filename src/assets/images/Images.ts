09b99066907f05b03dfa3ecf89b505912467b1e9
CuDuyDuc <duccu1403@gmail.com>
2024-02-05 16:13:38 +0700

    first commit

---
+++ src/assets/images/Images.ts
@@ -0,0 +1,15 @@
+const IMAGES = {
+    Splash_Logo: require('./splash_screen.png'),
+    Coffe_Onboarding: require('./coffee_onboarding2.png'),
+    Coffe_Onboarding1: require('./coffee_onboarding.png'),
+    Coffe_Onboarding2: require('./coffee_onboarding1.png'),
+    FbIcon : require('./icon_fb.png'),
+    GgIcon: require('./icon_gg.png'),
+    LogoLogin : require('./login.png'),
+    Home : require('./home.png'),
+    Cart : require('./cart.png'),
+    Favorites : require('./favorites.png'),
+    OderHistory : require('./order_history.png'),
+}
+
+export default IMAGES;
